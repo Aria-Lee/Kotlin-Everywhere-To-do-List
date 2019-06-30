594e9452f119bd6b3903250f357674551530b7c0
goldenf.android <lester.lee@goldenf.co>
2019-06-30 21:05:17 +0800

    finish add and edit.

---
+++ app/src/main/java/com/example/kotlin_everywhere_to_do_list/EditActivity.kt
@@ -0,0 +1,37 @@
+package com.example.kotlin_everywhere_to_do_list
+
+import android.app.Activity
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_edit.*
+
+class EditActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit)
+
+        title = "編輯模式"
+
+        if (!intent.getStringExtra("event").isNullOrEmpty()) edtText_edit.setText(intent.getStringExtra("event"))
+
+        btn_confirm.setOnClickListener {
+            if (edtText_edit.text.isNullOrEmpty()) Toast.makeText(this, "請輸入待辦事項", Toast.LENGTH_SHORT).show()
+            else confirm()
+        }
+
+        btn_cancel.setOnClickListener {
+            this.finish()
+        }
+
+    }
+
+    private fun confirm(){
+        val newEvent = edtText_edit.text.toString()
+        val intent = Intent().putExtra("newEvent", newEvent)
+        setResult(Activity.RESULT_OK, intent)
+        finish()
+    }
+}
---
+++ app/src/main/java/com/example/kotlin_everywhere_to_do_list/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.kotlin_everywhere_to_do_list
+
+import android.app.Activity
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var isAddMode = true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_add.setOnClickListener {
+            if (isAddMode) addEvent()
+            else Toast.makeText(this, "已經新增過了，不能再新增囉。", Toast.LENGTH_SHORT).show()
+        }
+
+        btn_edit.setOnClickListener {
+            if (!isAddMode) editEvent()
+            else Toast.makeText(this, "請先新增後才能編輯喔！", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun addEvent(){
+        val intent = Intent(this, EditActivity::class.java)
+        startActivityForResult(intent, 1)
+    }
+
+    private fun editEvent(){
+        val intent = Intent(this, EditActivity::class.java)
+        val event = tv_display.text
+        intent.putExtra("event", event)
+        startActivityForResult(intent, 1)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK){
+            tv_display.text = data?.getStringExtra("newEvent")
+            isAddMode = false
+        }
+    }
+}
